c55afcba1ae6898bc94947e9b481228e3921ff10
yakhlafhoussam <yakhlafhoussam@gmail.com>
2026-01-01 23:04:10 +0100

    KAN-12 Prepare the structure of the folders and add schema sql

---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE EchoBlog;
+
+USE EchoBlog;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    role ENUM ('author', 'admin', 'reader') DEFAULT 'reader' NOT NULL
+);
+
+CREATE TABLE articles(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author_id int NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+create table likes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    article_id int NOT NULL,
+    reader_id int NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE,
+    FOREIGN KEY (reader_id) REFERENCES users(id) ON DELETE CASCADE
+);
